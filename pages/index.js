cadea550bd88e7aa49ac654c07a51b861ebede14
hilalsidhic <hilalsidhic21@gmail.com>
2022-01-31 05:20:35 +0530

    pages added

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,10 @@
+import Link from "next/link";
 function HomePage() {
-    return <div>Welcome To Next JS!</div>;
+    return(
+        <>
+        <h1>Home Page</h1>
+        <Link href="posts/first">Click here to go to first post</Link>
+        </>
+    )
 }
 export default HomePage;
---
+++ pages/posts/first.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+function firstpage() {
+    return(
+        <div>
+            <h1>First Page</h1>
+            <Link href="/">Click Here to go to home page</Link>
+        </div>
+    )
+}
+export default firstpage;
